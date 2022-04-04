f734b5721a59ac44b9e70f0e4287ebf0973bad75
chaeri93 <chaeri93@ewhain.net>
2022-04-04 16:53:23 +0900

    model 생성

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
+from django.contrib.gis import admin
 
-# Register your models here.
+from .models import Mark
+
+
+@admin.register(Mark)
+class MarkAdmin(admin.OSMGeoAdmin):
+    list_display = ("name", "location")
--- disaster_map/settings.py
+++ disaster_map/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
+    'rest_framework',
+    'rest_framework_gis',
 ]
 
 MIDDLEWARE = [
