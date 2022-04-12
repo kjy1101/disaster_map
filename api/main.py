99d29d6a0dd1c10c26f7a61b84a1da8a6fe9f433
Jaeyeon Kim <amelia1023@naver.com>
2022-04-12 15:45:23 +0900

    .

--- api/main.py
+++ api/main.py
@@ -1,21 +1,7 @@
-from time import time
-#from twitter_api import twitter_api_api, utc2kst, removeRT
 import time
-from datetime import datetime
-#from twitter_api import *
 import twitter_api
 
 def main(interval=60):
-    """query = ["지성"]
-    stream = twitter_api_api.GetStreamFilter(track=query)
-    times = []
-
-    for tweets in stream:
-        print('***********crawling***********')
-        if removeRT(tweets['text']):
-            time = utc2kst(tweets['created_at'])
-            times.append(time)
-            print(tweets)"""
 
     while True:
         t0 = time.time()
@@ -28,8 +14,6 @@ def main(interval=60):
         twids,times,texts,users = twitter_api.search_tweets(queries)
 
         time.sleep(60) # 60초에 한번씩 실행
-    
-    print("out")
 
 
 if __name__ == '__main__':
