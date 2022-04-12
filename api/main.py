815037218677a52a97b31a5b7b7e6880e7d6c9ce
Jaeyeon Kim <kjyeon1101@ninewatt.com>
2022-04-12 15:02:42 +0900

    retrieve tweets 1 min

--- api/main.py
+++ api/main.py
@@ -1,7 +1,12 @@
-from twitter_api import twitter_api_api, utc2kst, removeRT
+from time import time
+#from twitter_api import twitter_api_api, utc2kst, removeRT
+import time
+from datetime import datetime
+#from twitter_api import *
+import twitter_api
 
 def main(interval=60):
-    query = ["지성"]
+    """query = ["지성"]
     stream = twitter_api_api.GetStreamFilter(track=query)
     times = []
 
@@ -10,7 +15,21 @@ def main(interval=60):
         if removeRT(tweets['text']):
             time = utc2kst(tweets['created_at'])
             times.append(time)
-            print(tweets)
+            print(tweets)"""
+
+    while True:
+        t0 = time.time()
+        t_tot = time.time()-t0
+        print("***")
+        print(time.time())
+
+        # 트윗 가져오기
+        queries = ["산불"]
+        twids,times,texts,users = twitter_api.search_tweets(queries)
+
+        time.sleep(60) # 60초에 한번씩 실행
+    
+    print("out")
 
 
 if __name__ == '__main__':
--- api/twitter_api.py
+++ api/twitter_api.py
@@ -17,16 +17,31 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # main code start
 import twitter, time
 
+# twitter api connect
 twitter_consumer_key = env('twitter_consumer_key')
 twitter_consumer_secret = env('twitter_consumer_secret')
 twitter_access_token = env('twitter_access_token')
 twitter_access_secret = env('twitter_access_secret')
 
-twitter_api_api = twitter.Api(consumer_key=twitter_consumer_key,
+twitterAPI = twitter.Api(consumer_key=twitter_consumer_key,
                           consumer_secret=twitter_consumer_secret, 
                           access_token_key=twitter_access_token, 
                           access_token_secret=twitter_access_secret)
 
+# remove RT tweets
+def removeRT(text):
+
+    if text[0] == 'R' and text[1] == 'T':
+        return False
+    else:
+        return True
+
+# change to korean time
+def utc2kst(utc_str):
+    ust = datetime.strptime(utc_str, '%a %b %d %H:%M:%S +0000 %Y')
+    kst = ust + timedelta(hours=9)
+    kst_str = kst.strftime('%Y.%m.%d %H:%M:%S')
+    return kst_str
 
 # 자연재해마다 사용할 검색 키워드 queries에 모두 리스트로 저장해서 넘김
 # ex. queries= ["지진", "earthquake", "진동", "흔들렸"]
@@ -38,7 +53,7 @@ def search_tweets(queries, sids=None):
     texts = [] # text (텍스트)
     users = [] # user name (유저 이름)
 
-    stream = twitter_api_api.GetStreamFilter(track=queries)
+    stream = twitterAPI.GetStreamFilter(track=queries)
 
     delay = 60 * 1 # 60 seconds * 1 minutes
     close_time = time.time() + delay
@@ -47,7 +62,7 @@ def search_tweets(queries, sids=None):
     for tweets in stream:
         print(tweets['text'])
         print('----------------------------------')
-        times.append(tweets['created_at'])
+        times.append(utc2kst(tweets['created_at']))
         texts.append(tweets['text'])
         twids.append(tweets['id_str'])
         users.append(tweets['user']['name'])
@@ -60,18 +75,5 @@ def search_tweets(queries, sids=None):
     return twids, times, texts, users
 
 
-query = ["산불", "지진", "태풍", "홍수", "가뭄", "자연재해", "화산", "화재"]
-search_tweets(query)
-
-def removeRT(text):
-
-    if text[0] == 'R' and text[1] == 'T':
-        return False
-    else:
-        return True
-
-def utc2kst(utc_str):
-    ust = datetime.strptime(utc_str, '%a %b %d %H:%M:%S +0000 %Y')
-    kst = ust + timedelta(hours=9)
-    kst_str = kst.strftime('%Y.%m.%d %H:%M:%S')
-    return kst_str
+# query = ["산불", "지진", "태풍", "홍수", "가뭄", "자연재해", "화산", "화재"]
+# search_tweets(query)
